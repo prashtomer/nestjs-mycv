1740ccc72c45d649c9ff79d77be3b7bb1cbf2838
prashtomer <prash.tomer@gmail.com>
2023-01-22 12:50:53 +0530

    Filter out email from the find user response using the custom interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -5,19 +5,21 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     handler: CallHandler<any>,
   ): Observable<any> {
-    // Run something before the request is handled by the request handler
-    console.log('I am running before the handler', context);
-
     return handler.handle().pipe(
       map((data: any) => {
-        // Run something before the response is sent out
-        console.log('I am running before the response is sent out', data);
+        // convert the User entity instance ie data into User dto
+        return plainToClass(UserDto, data, {
+          // will take care of properties marked as exposed inside the User dto and rest is stripped off from the response ie email
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
