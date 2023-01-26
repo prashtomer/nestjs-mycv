cf4fc30e29c2787f339c2381f7ada8bbfe5192dd
prashtomer <prash.tomer@gmail.com>
2023-01-26 18:10:12 +0530

    Some minor config changes for production deployment and delte redundant code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // process.env.PORT is environment variable provided by heroku
+  // for production deployment on heroku
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
